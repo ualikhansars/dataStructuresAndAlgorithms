ae2cb6491d095de53ea6daa81754f365e0f3dac7
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-12-01 05:56:36 -0800

    implement min, max and successor

--- dataStructuresAndAlgorithms/BinarySearchTree.cpp
+++ dataStructuresAndAlgorithms/BinarySearchTree.cpp
@@ -1,5 +1,17 @@
 #include "BinarySearchTree.h"
 
+void BinarySearchTree::Transplant(TreeNode* u, TreeNode* v)
+{
+	if (u->parent == nullptr)
+		root = v;
+	else if (u == u->parent->left)
+		u->parent->left = v;
+	else
+		u->parent->right = v;
+	if (v != nullptr)
+		v->parent = u->parent;
+}	
+
 BinarySearchTree::BinarySearchTree(): root(nullptr)
 {
 }
@@ -16,27 +28,56 @@ TreeNode* BinarySearchTree::Search(TreeNode* root, int key)
 void BinarySearchTree::Insert(int value)
 {
 	TreeNode newNode = TreeNode(value);
-	TreeNode* y = nullptr;
+	TreeNode* parent = nullptr;
 	TreeNode* x = root;
 	
 	while (x != nullptr)
 	{
-		y = x;
+		parent = x;
 		if (newNode.key < x->key) 
 			x = x->left;
 		else 
 			x = x->right;
 	}
-	newNode.p = y;
-	if (y == nullptr)
+	newNode.parent = parent;
+	if (parent == nullptr)
 		root = &newNode;
-	else if (newNode.key < y->key)
-		y->left = &newNode;
+	else if (newNode.key < parent->key)
+		parent->left = &newNode;
 	else
-		y->right = &newNode;
+		parent->right = &newNode;
 }
 
-TreeNode::TreeNode(int key, TreeNode* left = nullptr, TreeNode* right = nullptr, TreeNode* p = nullptr)
-	: key(key), left(left), right(right), p(p)
+TreeNode* BinarySearchTree::Minumum(TreeNode* node)
 {
+	while (node->left != nullptr) {
+		node = node->left;
+	}
+	return node;
+}
+
+TreeNode* BinarySearchTree::Maximum(TreeNode* node)
+{
+	while (node->right != nullptr) {
+		node = node->right;
+	}
+	return node;
+}
+
+TreeNode* BinarySearchTree::Successor(TreeNode* node)
+{
+	if (node->right != nullptr)
+		return Minumum(node->right);
+	TreeNode* parent = node->parent;
+	while (parent != nullptr and node == parent->right) {
+		node = parent;
+		parent = parent->parent;
+	}
+
+	return parent;
+}
+
+TreeNode::TreeNode(int key, TreeNode* left, TreeNode* right, TreeNode* p)
+	: key(key), left(left), right(right), parent(p)
+{ 
 }
--- dataStructuresAndAlgorithms/BinarySearchTree.h
+++ dataStructuresAndAlgorithms/BinarySearchTree.h
@@ -1,22 +1,33 @@
 #pragma once
 
+struct TreeNode {
+	int key;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode* parent;
+
+	TreeNode() = delete;
+
+	TreeNode(int key, TreeNode* left = nullptr, TreeNode* right = nullptr, TreeNode* p = nullptr);
+};
+
 class BinarySearchTree {
-	TreeNode* root;
+	TreeNode* root = nullptr;
+
+	void Transplant(TreeNode* u, TreeNode* v);
 
 public:
+
 	BinarySearchTree();
 
 	TreeNode* Search(TreeNode* root, int key);
 
 	void Insert(int value);
+
+	TreeNode* Minumum(TreeNode* node);
+
+	TreeNode* Maximum(TreeNode* node);
+
+	TreeNode* Successor(TreeNode* node);
 };
 
-struct TreeNode {
-	int key;
-	TreeNode* left;
-	TreeNode* right;
-	TreeNode* p;
-
-public:
-	TreeNode(int key, TreeNode* left = nullptr, TreeNode* right = nullptr, TreeNode* p = nullptr){}
-};
